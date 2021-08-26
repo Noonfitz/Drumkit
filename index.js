785d56cc9165ddbcfadabd0379adaf53dd93498d
Noonfitz <87461321+Noonfitz@users.noreply.github.com>
2021-08-26 03:13:48 -0400

    initial files

---
+++ index.js
@@ -0,0 +1,70 @@
+// detecting button press
+
+var lengthOfDrums = document.querySelectorAll(".drum").length;
+
+for(var i=0; i<lengthOfDrums; i++){
+
+document.querySelectorAll("button")[i].addEventListener("click",function(){
+var buttonInner= this.innerHTML;
+makeSound(buttonInner);
+buttonAnimation(buttonInner);
+
+});
+
+}
+
+
+// detecting keybord press
+document.addEventListener("keypress",function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+
+
+
+
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+    var tom1 = new Audio("sounds/tom-1.mp3");
+    tom1.play();
+      break;
+      case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+        break;
+        case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+          break;
+          case "d":
+          var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+            break;
+            case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+              break;
+              case "k":
+              var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+                break;
+                case "l":
+                var kick= new Audio("sounds/kick-bass.mp3");
+              kick.play();
+                  break;
+    default: console.log(key);
+  }
+
+}
+
+function buttonAnimation(currentKey){
+  var activeKeyPress = document.querySelector("."+ currentKey);
+  activeKeyPress.classList.add("pressed");
+setTimeout(function(){
+  activeKeyPress.classList.remove("pressed");
+}, 100);
+
+}
